138b11b40e762e5b78076cb10948e451a7dd442b
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-02-23 23:22:26 +0900

    fix assets-precompile error

--- app/assets/javascripts/feeds_show.js
+++ app/assets/javascripts/feeds_show.js
@@ -1,13 +1,10 @@
 $(function () {
     $('.isg_feed_show_menu').click(function(e) {
-
 	let HEADER_HEIGHT=0;
 	let HEADER_MARGIN=10;
 	let TEXTAREA_HEIGHT=144;
 	let TEXTAREA_MARGIN=10;
 	
-        //console.log(e.originalEvent);
-        
         let ul=$(this).next('ul');
         let top=e.originalEvent.pageY-(HEADER_HEIGHT-HEADER_MARGIN)-(TEXTAREA_HEIGHT-TEXTAREA_MARGIN);
         let right=96;
@@ -15,6 +12,4 @@ $(function () {
         ul.css('right', right);        
         ul.slideToggle();        
     });
-
-    
 });
