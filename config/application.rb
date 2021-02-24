94c830f16a7a845ebcd122079cddd2c46f1d972a
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-02-24 14:03:05 +0900

    config setting to application.rb NOT to auto-generate assets files.

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,21 @@ Bundler.require(*Rails.groups)
 
 module InstaClone
   class Application < Rails::Application
+    config.time_zone = 'Asia/Tokyo'
+    #config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-  config.time_zone = 'Asia/Tokyo'
   end
 end
